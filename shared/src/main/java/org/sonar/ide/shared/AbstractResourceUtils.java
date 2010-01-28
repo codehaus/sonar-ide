fe9ad772e4e544b0f3959ac6afd6583361c250e4
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-01-28 18:00:55 +0000

    Add JavaDoc about resource keys.

--- shared/src/main/java/org/sonar/ide/shared/AbstractResourceUtils.java
+++ shared/src/main/java/org/sonar/ide/shared/AbstractResourceUtils.java
@@ -4,15 +4,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * <ul>
+ * <li><strong>Java file:</strong> groupId:artifactId:packageName:fileNameWithoutExt
+ * <ul>
+ * <li>test:test:org.sonar:Foo</li>
+ * <li>test:test:[default]:Bar</li>
+ * </ul>
+ * </li>
+ * <li><strong>File:</strong> groupId:artifactId:directoryPath/fileName
+ * <ul>
+ * <li>test:test:org/sonar/foo.sql</li>
+ * <li>test:test:[root]/bar.sql</li>
+ * </ul>
+ * </li>
+ *
  * @author Evgeny Mandrikov
  */
 public abstract class AbstractResourceUtils<MODEL> {
   private static final Logger LOG = LoggerFactory.getLogger(AbstractResourceUtils.class);
 
   /**
-   * Default package name for classes without package definition
+   * Default package name for classes without package definition.
    */
   public static final String DEFAULT_PACKAGE_NAME = "[default]";
+
+  public static final String ROOT = "[root]";
 
   public static final char DELIMITER = ':';
 
