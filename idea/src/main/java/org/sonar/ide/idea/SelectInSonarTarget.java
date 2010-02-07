a22f084c91c71f7153ff17c2716321393d46633f
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-02-07 21:05:00 +0000

    SONARIDE-18	Add "go to Sonar dashboard" action

---
+++ idea/src/main/java/org/sonar/ide/idea/SelectInSonarTarget.java
@@ -0,0 +1,58 @@
+package org.sonar.ide.idea;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.ide.SelectInContext;
+import com.intellij.ide.SelectInTarget;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nullable;
+import org.sonar.ide.idea.utils.SonarUtils;
+
+/**
+ * @author Evgeny Mandrikov
+ */
+public class SelectInSonarTarget implements SelectInTarget {
+  @Override
+  public boolean canSelect(SelectInContext context) {
+    Object selector = context.getSelectorInFile();
+    Project project = context.getProject();
+    return getSonarUrl(selector, project) != null;
+  }
+
+  @Override
+  public void selectIn(SelectInContext context, boolean requestFocus) {
+    Object selector = context.getSelectorInFile();
+    Project project = context.getProject();
+    String url = getSonarUrl(selector, project);
+    if (url != null) {
+      BrowserUtil.launchBrowser(url);
+    }
+  }
+
+  @Nullable
+  private String getSonarUrl(Object selector, Project project) {
+    // TODO implement go to project, go to package, go to file
+    return SonarUtils.getSonarSettings(project).getServer().getHost();
+  }
+
+  @Override
+  public float getWeight() {
+    return Float.MAX_VALUE;
+  }
+
+  @Nullable
+  @Override
+  public String getToolWindowId() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getMinorViewId() {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return "Sonar";
+  }
+}
