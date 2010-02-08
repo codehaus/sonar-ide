5611c261aceb015fd0c34183896976d708058080
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-02-08 14:35:24 +0000

    Minor fixes.

--- idea/src/main/java/org/sonar/ide/idea/utils/IdeaIconLoader.java
+++ idea/src/main/java/org/sonar/ide/idea/utils/IdeaIconLoader.java
@@ -1,6 +1,7 @@
 package org.sonar.ide.idea.utils;
 
 import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.annotations.Nullable;
 import org.sonar.ide.ui.AbstractIconLoader;
 
 import javax.swing.*;
@@ -10,7 +11,10 @@ import javax.swing.*;
  */
 public final class IdeaIconLoader extends AbstractIconLoader {
   @Override
-  public Icon getIcon(String path) {
+  public Icon getIcon(@Nullable String path) {
+    if (path == null) {
+      return null;
+    }
     return IconLoader.getIcon(path);
   }
 }
