6d967f0b75094f21d8d8234f52e48160e71e7ff9
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-02-14 21:56:16 +0000

    SONARIDE-22: Add TreeMap component

---
+++ shared/src/main/java/org/sonar/ide/ui/treemap/split/Squarified.java
@@ -0,0 +1,18 @@
+package org.sonar.ide.ui.treemap.split;
+
+import org.sonar.ide.ui.treemap.AbstractSplitStrategy;
+import org.sonar.ide.ui.treemap.TreeMapNode;
+
+import java.util.List;
+
+/**
+ * <a href="http://www.win.tue.nl/~vanwijk/stm.pdf">Squarified Treemaps (Mark Bruls, Kees Huizing, and Jarke J. van Wijk)</a>
+ *
+ * @author Evgeny Mandrikov
+ */
+public class Squarified extends AbstractSplitStrategy {
+  @Override
+  protected void split(List<TreeMapNode> items, List<TreeMapNode> r1, List<TreeMapNode> r2) {
+    // TODO
+  }
+}
