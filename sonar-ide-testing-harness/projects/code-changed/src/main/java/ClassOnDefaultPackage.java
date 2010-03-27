22bd3c708d0e7fd758921b0b1fde17bf1770ddf9
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-03-27 22:16:59 +0000

    SONARIDE-53: Add projects for sonar-ide-testing-harness

---
+++ sonar-ide-testing-harness/projects/code-changed/src/main/java/ClassOnDefaultPackage.java
@@ -0,0 +1,12 @@
+public class ClassOnDefaultPackage {
+
+  public ClassOnDefaultPackage(int i) {
+    /* Next line was changed */
+    //int j = i++;
+  }
+
+  /* Next method was renamed */
+  private String myMethod() {
+    return "hello";
+  }
+}
