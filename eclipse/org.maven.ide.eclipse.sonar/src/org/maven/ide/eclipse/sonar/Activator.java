ee31d6633647a592678c42ead2a1e9de9d600048
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-04-30 23:46:29 +0000

    [maven-release-plugin]  copy for tag sonar-ide-shared-components-0.1

--- eclipse/org.maven.ide.eclipse.sonar/src/org/maven/ide/eclipse/sonar/Activator.java
+++
@@ -1,7 +0,0 @@
-package org.maven.ide.eclipse.sonar;
-
-import org.eclipse.core.runtime.Plugin;
-
-public class Activator extends Plugin {
-  public static final String PLUGIN_ID = "org.maven.ide.eclipse.sonar";
-}
--- eclipse/update-dependencies.bat
+++
@@ -1,1 +0,0 @@
-mvn -o -Dtycho.mode=maven -Pupdate-dependencies clean org.sonatype.tycho:maven-osgi-packaging-plugin:generate-bundle
