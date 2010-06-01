33bba6cf55acbd64dd0a8c4e3bd8c4de94caa7a8
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-06-01 08:36:42 +0000

    Add module sonar-book

---
+++ sonar-book/src/main/javahelp/idea-helpset.hs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="ISO-8859-1" standalone="no"?>
+
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<helpset version="1.0">
+   <title>Sonar</title>
+   <maps>
+      <homeID>top</homeID>
+      <mapref location="jhelpmap.jhm"/>
+   </maps>
+   <view>
+      <name>TOC</name>
+      <label>Table Of Contents</label>
+      <type>javax.help.TOCView</type>
+      <data>jhelptoc.xml</data>
+   </view>
+</helpset>
