ed26dd1230bf96e16bd3c39f448fd91e0119e37a
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-06-12 19:51:33 +0000

    SONARIDE-67: Show measures

--- shared/sonar-ide-commons/src/main/java/org/sonar/ide/shared/measures/MeasuresLoader.java
+++ shared/sonar-ide-commons/src/main/java/org/sonar/ide/shared/measures/MeasuresLoader.java
@@ -1,12 +1,16 @@
 package org.sonar.ide.shared.measures;
 
-import org.sonar.wsclient.Sonar;
-import org.sonar.wsclient.services.*;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.sonar.wsclient.Sonar;
+import org.sonar.wsclient.services.Measure;
+import org.sonar.wsclient.services.Metric;
+import org.sonar.wsclient.services.MetricQuery;
+import org.sonar.wsclient.services.Resource;
+import org.sonar.wsclient.services.ResourceQuery;
 
 /**
  * @author Evgeny Mandrikov
@@ -21,8 +25,10 @@ public final class MeasuresLoader {
    * @return measures
    */
   public static List<MeasureData> getMeasures(Sonar sonar, String resourceKey) {
+    // TODO Godin: This is not optimal. Would be better to load metrics only once.
     List<Metric> metrics = sonar.findAll(MetricQuery.all());
     Map<String, Metric> metricsByKey = new HashMap<String, Metric>();
+    // TODO Godin: We shouldn't load all measures.
     for (Metric metric : metrics) {
       metricsByKey.put(metric.getKey(), metric);
     }
@@ -34,7 +40,7 @@ public final class MeasuresLoader {
     for (Measure measure : resource.getMeasures()) {
       Metric metric = metricsByKey.get(measure.getMetricKey());
       result.add(new MeasureData()
-          .setName(measure.getMetricName())
+          .setName(metric.getName())
           .setDomain(metric.getDomain())
           .setValue(measure.getFormattedValue())
       );
