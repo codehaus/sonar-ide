ba2430f6aea672d232364475ba05fd35f300fa75
godin <godin@99cbfe3a-a47d-0410-a3ab-d8de1ee668d8>
2010-06-22 16:03:46 +0000

    Minor fix.

--- shared/sonar-ide-commons/src/main/java/org/sonar/ide/api/SonarIdeException.java
+++ shared/sonar-ide-commons/src/main/java/org/sonar/ide/api/SonarIdeException.java
@@ -22,14 +22,18 @@ package org.sonar.ide.api;
  * Runtime exception.
  * Good practice is to always specify detail message for exception,
  * so there is no constructors without parameter "message".
- *
+ * 
  * @author Evgeny Mandrikov
  */
 public class SonarIdeException extends RuntimeException {
+
+  private static final long serialVersionUID = 5153419702918423973L;
+
   /**
    * Constructs a new runtime exception with the specified detail message.
-   *
-   * @param message the detail message
+   * 
+   * @param message
+   *          the detail message
    */
   public SonarIdeException(String message) {
     super(message);
@@ -37,9 +41,11 @@ public class SonarIdeException extends RuntimeException {
 
   /**
    * Constructs a new runtime exception with the specified detail message and cause.
-   *
-   * @param message the detail message
-   * @param cause   the cause
+   * 
+   * @param message
+   *          the detail message
+   * @param cause
+   *          the cause
    */
   public SonarIdeException(String message, Throwable cause) {
     super(message, cause);
